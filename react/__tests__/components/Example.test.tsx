2fa392fbbd109d90f8b980afe6baa41e3cf7a3ea
˜Hamilton <hamilton_dino02@hotmail.com>
2023-05-26 13:40:00 -0300

    feat: few changes at test file

--- react/__tests__/components/Example.test.tsx
+++ react/__tests__/components/Example.test.tsx
@@ -23,28 +23,7 @@ describe('first test', () => {
     jest.resetAllMocks()
   })
 
-  it('test one', async () => {
-    mockedUseProduct.mockReturnValue({ product: { productId: 123 } } as any)
-    global.fetch = mockFetch({
-      recipes: [
-        {
-          id: 'string',
-          skuRecipe: 2,
-          postTitle: 'string',
-          postContent: 'string',
-          postDate: 'string',
-        },
-      ],
-    })
-
-    await act(async () => {
-      renderComponent()
-    })
-
-    expect(screen.getByText('Posts for this product:')).toBeInTheDocument()
-  })
-
-  it('test two', async () => {
+  it('should render "No products found." if product is null', async () => {
     mockedUseProduct.mockReturnValue({ product: null } as any)
     await act(async () => {
       renderComponent()
@@ -53,7 +32,7 @@ describe('first test', () => {
     expect(screen.getByText('No products found.')).toBeInTheDocument()
   })
 
-  it('test three', async () => {
+  it('should render "No posts for this product." if recipes is empty', async () => {
     mockedUseProduct.mockReturnValue({ product: { productId: 123 } } as any)
 
     global.fetch = mockFetch({
@@ -65,5 +44,41 @@ describe('first test', () => {
     })
 
     expect(screen.getByText('No posts for this product.')).toBeInTheDocument()
+  })
+
+  it('should render the recipes if there are any', async () => {
+    mockedUseProduct.mockReturnValue({ product: { productId: 123 } } as any)
+    global.fetch = mockFetch({
+      recipes: [
+        {
+          id: '1',
+          postTitle: 'Recipe 1',
+          postDate: '2022-01-01',
+          postContent: 'Lorem ipsum dolor sit amet',
+          skuRecipe: 123,
+        },
+        {
+          id: '2',
+          postTitle: 'Recipe 2',
+          postDate: '2022-01-02',
+          postContent: 'Consectetur adipiscing elit',
+          skuRecipe: 456,
+        },
+      ],
+    })
+
+    await act(async () => {
+      renderComponent()
+    })
+
+    expect(screen.getByText('Posts for this product:')).toBeInTheDocument()
+    expect(screen.getByText('Recipe 1')).toBeInTheDocument()
+    expect(screen.getByText('2022-01-01')).toBeInTheDocument()
+    expect(screen.getByText('Lorem ipsum dolor sit amet')).toBeInTheDocument()
+    expect(screen.getByText('Sku da receita: 123')).toBeInTheDocument()
+    expect(screen.getByText('Recipe 2')).toBeInTheDocument()
+    expect(screen.getByText('2022-01-02')).toBeInTheDocument()
+    expect(screen.getByText('Consectetur adipiscing elit')).toBeInTheDocument()
+    expect(screen.getByText('Sku da receita: 456')).toBeInTheDocument()
   })
 })
